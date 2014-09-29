af12f947587ddcb1bd419dac4f66d52bb990ec36
Tyrone <tdubs241083@gmail.com>
2014-09-29 21:48:36 +0200

    some changes to the photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,21 @@
 class PhotosController < ApplicationController
 
   def new
+    #create a photo object to be rendered in the form
+    @photo = Shoot.find(params[:id]).photos.new
   end
 
   def create
-    @photo = Shoots.photos.build(photo_params)
-    @photo.save
+    #try create the new photo record
+    @photo = Shoot.find(params[:id]).photos.create(photo_params)
+    #if it gets persisted
+    if @photos.persisted?
+      flash[:success] = "You have successfully created the photo"
+      redirect_to photo_path @photos
+    else
+      #render the new template with errors showing if any
+      render 'new'
+    end
   end
 
   def edit
