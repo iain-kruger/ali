d8447df2b34aa8e17c00b32041dc5e2fff63e702
Tyrone <tdubs241083@gmail.com>
2014-09-29 21:40:55 +0200

    annotate models and use validates_presence_of

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,15 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  shoot_id   :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Photo < ActiveRecord::Base
 	belongs_to :shoot
-  validates :shoot_id, presence: true
+  validates_presence_of :shoot
 end
