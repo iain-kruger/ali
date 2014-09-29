5372e2c4947d3dd5a9363ea6abd97a4d7b88f14f
Tyrone <tdubs241083@gmail.com>
2014-09-29 21:42:01 +0200

    remove routes provided already by the resources calls

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,14 @@ Ali::Application.routes.draw do
   match '/gallery', to: 'static_pages#gallery',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/shoots', to: 'shoots#show', via: 'get'
    match '/shoots/showadmin/:shootName', to: 'shoots#showadmin', via: 'get'
 
   match '/photos/:shootName', to: 'shoots#photos', via: 'get'
   match '/show', to: 'shoots#show', via: 'get'
   get '/shoots/go/:catagory', to: 'shoots#show'
-  match '/index', to: 'shoots#index', via: 'get'
- 
- 
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
