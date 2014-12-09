9dbcbfa47be0bb024a2e4ee19ff664f9c8311294
Iain Kruger <iain.kruger@gmail.com>
2014-12-09 13:46:32 +0200

    dropbox correction

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
-  config.dropbox_app_key = "0nkkz1gq48348q9"
+  config.dropbox_app_key = "0nkkz1gq48348q9" 
   config.dropbox_app_secret = "4wtbcwxbiysgvvu"
-  config.dropbox_access_token = "vhoutleiyz84tab4"
-  config.dropbox_access_token_secret = "hmo7soqp7r7pal9s"
+  config.dropbox_access_token = "0jhkuetej7cn8y7p"
+  config.dropbox_access_token_secret = "kvblv0nq9eo6rnw"
   config.dropbox_user_id = "358908665"
   config.dropbox_access_type = "app_folder"
 end
